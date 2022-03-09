c1a3a62e5b57aede8cff9d6bee3c4a90daeb37bc
zdgeorgi <zdgeorgi@cisco.com>
2022-03-09 17:49:57 +0000

    Adding the csv files

---
+++ endpoints/default.py
@@ -0,0 +1,9 @@
+from fastapi import APIRouter
+from starlette.responses import RedirectResponse
+
+router = APIRouter()
+
+
+@router.get("/", name="Redirects to docs", include_in_schema=False, tags=["Default"])
+async def docs_redirect() -> RedirectResponse:
+    return RedirectResponse(url="/docs")
